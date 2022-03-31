024beb784b8af3036b40388e18cbf70ab8b55b04
bigen <bigen1925@gmail.com>
2022-04-01 07:23:06 +0900

    fix サイドバーのコンポーネントをApp.jsから切り出した

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -1,103 +1,21 @@
-import Button from '@mui/material/Button';
-
 import react, { useState } from 'react';
 import { BrowserRouter, Router, Route, Routes } from 'react-router-dom';
 
 import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
 import CssBaseline from '@mui/material/CssBaseline';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
-
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import HomeIcon from '@mui/icons-material/Home';
-import TagIcon from '@mui/icons-material/Tag';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import PetsIcon from '@mui/icons-material/Pets';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import SettingsIcon from '@mui/icons-material/Settings';
 
 import { Home } from './Home.js';
 import { Search } from './Search.js';
 import { Notification } from './Notification.js';
 import { Profile } from './Profile.js';
 import { Settings } from './Settings.js';
-
-const drawerWidth = 240;
+import { Sidebar } from './Sidebar.js';
 
 export function App() {
     return (
         <Box sx={{ display: 'flex' }}>
             <CssBaseline />
-            <Drawer
-                sx={{
-                    width: drawerWidth,
-                    flexShrink: 0,
-                    '& .MuiDrawer-paper': {
-                        width: drawerWidth,
-                        boxSizing: 'border-box',
-                    },
-                }}
-                variant="permanent"
-                anchor="left"
-            >
-                <Toolbar>
-                    {/* twitterに踏襲してるけどここ不要では */}
-                    <Button>
-                        <PetsIcon />
-                    </Button>
-                </Toolbar>
-                <List>
-                    {/* TODO: もっとシンプルにかけそう map使う */}
-                    <Link href="/">
-                        <ListItem button>
-                            <ListItemIcon>
-                                <HomeIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="home" />
-                        </ListItem>
-                    </Link>
-                    <Link href="/search">
-                        <ListItem button>
-                            <ListItemIcon>
-                                <TagIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="search" />
-                        </ListItem>
-                    </Link>
-                    <Link href="/notification">
-                        {/* notifications? */}
-                        <ListItem button>
-                            <ListItemIcon>
-                                <NotificationsIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="通知" />
-                        </ListItem>
-                    </Link>
-                    <Link href="/userid">
-                        <ListItem button>
-                            <ListItemIcon>
-                                <AccountCircleIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="プロフィール" />
-                        </ListItem>
-                    </Link>
-                    <Link href="/settings">
-                        <ListItem button>
-                            <ListItemIcon>
-                                <SettingsIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="設定" />
-                        </ListItem>
-                    </Link>
-                </List>
-                <Button variant="contained">ツイートする</Button>
-            </Drawer>
+            <Sidebar />
             <Box component="main" sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}>
                 {/* <Typography paragraph><Home />aaa</Typography> */}
                 <BrowserRouter>
---
+++ front/src/components/Sidebar.js
@@ -0,0 +1,86 @@
+import Drawer from '@mui/material/Drawer';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Link from '@mui/material/Link';
+import Button from '@mui/material/Button';
+
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import HomeIcon from '@mui/icons-material/Home';
+import TagIcon from '@mui/icons-material/Tag';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import PetsIcon from '@mui/icons-material/Pets';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import SettingsIcon from '@mui/icons-material/Settings';
+
+const drawerWidth = 240;
+
+export function Sidebar() {
+    return (
+        <Drawer
+            sx={{
+                width: drawerWidth,
+                flexShrink: 0,
+                '& .MuiDrawer-paper': {
+                    width: drawerWidth,
+                    boxSizing: 'border-box',
+                },
+            }}
+            variant="permanent"
+            anchor="left"
+        >
+            <Toolbar>
+                {/* twitterに踏襲してるけどここ不要では */}
+                <Button>
+                    <PetsIcon />
+                </Button>
+            </Toolbar>
+            <List>
+                {/* TODO: もっとシンプルにかけそう map使う */}
+                <Link href="/">
+                    <ListItem button>
+                        <ListItemIcon>
+                            <HomeIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="home" />
+                    </ListItem>
+                </Link>
+                <Link href="/search">
+                    <ListItem button>
+                        <ListItemIcon>
+                            <TagIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="search" />
+                    </ListItem>
+                </Link>
+                <Link href="/notification">
+                    {/* notifications? */}
+                    <ListItem button>
+                        <ListItemIcon>
+                            <NotificationsIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="通知" />
+                    </ListItem>
+                </Link>
+                <Link href="/userid">
+                    <ListItem button>
+                        <ListItemIcon>
+                            <AccountCircleIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="プロフィール" />
+                    </ListItem>
+                </Link>
+                <Link href="/settings">
+                    <ListItem button>
+                        <ListItemIcon>
+                            <SettingsIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="設定" />
+                    </ListItem>
+                </Link>
+            </List>
+            <Button variant="contained">ツイートする</Button>
+        </Drawer>
+    );
+}
