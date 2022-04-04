2d3ed1350d5ce9a4ad90e1ac2dedda9a7e880af4
Sayano <rng.jhn.osca@gmail.com>
2022-04-04 09:37:44 +0900

    delete 不要なコードの削除

--- api/controllers/zetter.js
+++ api/controllers/zetter.js
@@ -4,16 +4,6 @@ const getTweets = (req, res) => {
     res.json(tweets);
 };
 
-// const deleteProduct = (req, res) => {
-//     const id = Number(req.params.productID);
-//     const index = products.findIndex((product) => product.id === id);
-//     products.splice(index, 1);
-//     res.status(200).json('Product deleted');
-// };
-
 module.exports = {
     getTweets,
-    // getProduct,
-    // createProduct,
-    // deleteProduct,
 };
