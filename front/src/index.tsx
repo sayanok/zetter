5115682314b70b8b56207b17914fc42716c9d6db
Sayano <rng.jhn.osca@gmail.com>
2022-07-12 16:31:00 +0900

    add
    path追加

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -9,6 +9,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Sidebar from './Sidebar';
 import Login from './Login';
 import Home from './Home';
+import Tweet from './Tweet';
 import Profile from './Profile';
 import SettingsProfile from './SettingsProfile';
 
@@ -24,6 +25,7 @@ root.render(
                     <Routes>
                         <Route path="login" element={<Login />} />
                         <Route path="/" element={<Home />} />
+                        <Route path="tweet/:tweetId" element={<Tweet />} />
                         <Route path="profile" element={<Profile />} />
                         <Route path="settings/:username" element={<SettingsProfile />} />
                     </Routes>
