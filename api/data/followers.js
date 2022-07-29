0dc8e353d1f8ca225fc3a796618fedfdc45569a6
Sayano <rng.jhn.osca@gmail.com>
2022-07-29 13:53:00 +0900

    modify
    データ

--- api/data/followers.js
+++ api/data/followers.js
@@ -1,4 +1,7 @@
-let followers = [{ id: 1, userIdBeingFollowed: 3, followedUserId: 1, createdAt: new Date() }];
+let followers = [
+    { id: 1, userIdBeingFollowed: 3, followedUserId: 1, createdAt: new Date() },
+    { id: 2, userIdBeingFollowed: 3, followedUserId: 2, createdAt: new Date() },
+];
 // 日本語で書いてもややこしいけど
 // userIdBeingFollowed: フォローされている人
 // followedUserId: フォローしている人
