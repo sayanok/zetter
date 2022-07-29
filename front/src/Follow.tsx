73370d0c61244b3c65f9c60c0c972e27e1fe6022
Sayano <rng.jhn.osca@gmail.com>
2022-07-29 18:30:38 +0900

    modify
    フォロー中とフォロワーを見られるようにした

--- front/src/Follow.tsx
+++ front/src/Follow.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, SetStateAction } from 'react';
 import { useLocation, Link } from 'react-router-dom';
 import useCallApi from './utils/api';
 import { FollowerType } from './utils/types';
@@ -8,28 +8,45 @@ import ListItem from '@mui/material/ListItem';
 import Avatar from '@mui/material/Avatar';
 import ListItemText from '@mui/material/ListItemText';
 import Stack from '@mui/material/Stack';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
 
 const Follow: React.FC = () => {
     const callApi = useCallApi();
     const location = useLocation();
     const [usersList, setUsersList] = useState<Array<FollowerType>>([]);
+    // タブのためのstate
+    const [value, setValue] = React.useState(0);
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
 
     useEffect(() => {
         location.pathname === '/followings' ? getFollowings()?.then(setUsersList) : getFollowers()?.then(setUsersList);
-    }, []);
+    }, [location]);
 
     function getFollowings(): Promise<Array<FollowerType>> | undefined {
-        console.log('followings');
         return callApi('http://localhost:5000/api/zetter/followings');
     }
 
     function getFollowers(): Promise<Array<FollowerType>> | undefined {
-        console.log('followers');
         return callApi('http://localhost:5000/api/zetter/followers');
     }
 
     return (
         <>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                    {/* 修正の必要あり */}
+                    <Link to="/followings">
+                        <Tab label="フォロー" />
+                    </Link>
+                    <Link to="/followers">
+                        <Tab label="フォロワー" />
+                    </Link>
+                </Tabs>
+            </Box>
             <List sx={{ width: '100%', maxWidth: 1000, bgcolor: 'background.paper' }}>
                 {usersList.map((user, index) => (
                     <ListItem key={index} alignItems="flex-start">
