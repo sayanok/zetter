f5c0762fc97f5304b2a37490ec7293e9004b5d88
Sayano <rng.jhn.osca@gmail.com>
2022-07-29 11:10:36 +0900

    add
    フォロワーのデータを追加

---
+++ api/data/followers.js
@@ -0,0 +1,5 @@
+let followers = [{ id: 1, userIdBeingFollowed: 3, followedUserId: 1, createdAt: new Date() }];
+// 日本語で書いてもややこしいけど
+// userIdBeingFollowed: フォローされている人
+// followedUserId: フォローしている人
+module.exports = followers;
