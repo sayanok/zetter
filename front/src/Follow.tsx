2e56a3ecf40a225eb1d1b932d6f41418019fcfd6
bigen1925 <bigen1925@gmail.com>
2022-08-21 06:18:42 +0900

    参考実装

--- front/src/Follow.tsx
+++ front/src/Follow.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import useCallApi from './utils/api';
 import { FollowerType, ProfileType } from './utils/types';
 
@@ -14,205 +14,196 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-type FollowProps = { myProfile: ProfileType | undefined };
+const tabValues = ['followings', 'followers'] as const;
+type TabValue = typeof tabValues[number];
+
+type FollowProps = {
+  myProfile: ProfileType | undefined;
+  tabValue: TabValue;
+};
 
 const Follow: React.FC<FollowProps> = (props) => {
-    const callApi = useCallApi();
-    const params = useParams();
-    const [followingsList, setFollowingsList] = useState<Array<FollowerType>>([]);
-    const [followersList, setFollowersList] = useState<Array<FollowerType>>([]);
-    const [myFollowingsList, setMyFollowingsList] = useState<Array<FollowerType>>([]);
-    // タブのためのstate
-    const [value, setValue] = React.useState(0);
-    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-        setValue(newValue);
-    };
+  const callApi = useCallApi();
+  const params = useParams();
+  const navigate = useNavigate();
+  const [followingsList, setFollowingsList] = useState<Array<FollowerType>>([]);
+  const [followersList, setFollowersList] = useState<Array<FollowerType>>([]);
+  const [myFollowingsList, setMyFollowingsList] = useState<Array<FollowerType>>([]);
 
-    useEffect(() => {
-        getMyFollowings()?.then(setMyFollowingsList);
-        getFollowings()?.then(setFollowingsList);
-        getFollowers()?.then(setFollowersList);
-        // }, [location]);
-    }, []);
-
-    function getFollowings(): Promise<Array<FollowerType>> | undefined {
-        return callApi('http://localhost:5000/api/zetter/' + params.username + '/followings');
+  const handleChange = (event: React.SyntheticEvent, newTabIndex: number) => {
+    const newTabValue = tabValues[newTabIndex];
+    if (newTabValue !== props.tabValue) {
+      navigate(`/${params.username}/${newTabValue}`);
     }
+  };
 
-    function getFollowers(): Promise<Array<FollowerType>> | undefined {
-        return callApi('http://localhost:5000/api/zetter/' + params.username + '/followers');
-    }
+  useEffect(() => {
+    getMyFollowings()?.then(setMyFollowingsList);
+    getFollowings()?.then(setFollowingsList);
+    getFollowers()?.then(setFollowersList);
+    // }, [location]);
+  }, []);
 
-    function getMyFollowings(): Promise<Array<FollowerType>> | undefined {
-        return callApi('http://localhost:5000/api/zetter/' + props.myProfile?.username + '/followings');
-    }
+  function getFollowings(): Promise<Array<FollowerType>> | undefined {
+    return callApi('http://localhost:5000/api/zetter/' + params.username + '/followings');
+  }
 
-    function updateFollowings(username: string, order: string): void {
-        callApi('http://localhost:5000/api/zetter/updateFollowings', {
-            method: 'PATCH',
-            body: JSON.stringify({ followings: followingsList, followingUsername: username, order: order }),
-        });
-        afterUpdateFollowings();
-    }
+  function getFollowers(): Promise<Array<FollowerType>> | undefined {
+    return callApi('http://localhost:5000/api/zetter/' + params.username + '/followers');
+  }
 
-    function afterUpdateFollowings(): void {
-        getFollowings()?.then(setFollowingsList);
-        getMyFollowings()?.then(setMyFollowingsList);
-    }
+  function getMyFollowings(): Promise<Array<FollowerType>> | undefined {
+    return callApi('http://localhost:5000/api/zetter/' + props.myProfile?.username + '/followings');
+  }
 
-    // タブ関連のメソッドなど
-    interface TabPanelProps {
-        children?: React.ReactNode;
-        index: number;
-        value: number;
-    }
+  function updateFollowings(username: string, order: string): void {
+    callApi('http://localhost:5000/api/zetter/updateFollowings', {
+      method: 'PATCH',
+      body: JSON.stringify({ followings: followingsList, followingUsername: username, order: order }),
+    });
+    afterUpdateFollowings();
+  }
 
-    function TabPanel(props: TabPanelProps) {
-        const { children, value, index, ...other } = props;
+  function afterUpdateFollowings(): void {
+    getFollowings()?.then(setFollowingsList);
+    getMyFollowings()?.then(setMyFollowingsList);
+  }
 
-        return (
-            <div
-                role="tabpanel"
-                hidden={value !== index}
-                id={`simple-tabpanel-${index}`}
-                aria-labelledby={`simple-tab-${index}`}
-                {...other}
-            >
-                {value === index && (
-                    <Box sx={{ p: 3 }}>
-                        <Typography>{children}</Typography>
-                    </Box>
-                )}
-            </div>
-        );
-    }
+  // タブ関連のメソッドなど
+  interface TabPanelProps {
+    children?: React.ReactNode;
+    currentValue: TabValue;
+    tabValue: TabValue;
+  }
+
+  function TabPanel(props: TabPanelProps) {
+    const { children, currentValue, tabValue, ...other } = props;
 
     return (
-        <>
-            <Box sx={{ width: '100%' }}>
-                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                    <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-                        {/* 修正の必要あり */}
-                        <Tab
-                            label="フォロー中"
-                            //    href={'/' + params.username + '/followings'}
-                            //     onChange={(e) => {
-                            //         e.preventDefault();
-                            //     }}
-                        />
-                        <Tab
-                            label="フォロワー"
-                            //     href={'/' + params.username + '/followers'}
-                            //     onChange={(e) => {
-                            //         e.preventDefault();
-                            //     }}
-                        />
-                    </Tabs>
-                </Box>
-                <TabPanel value={value} index={0}>
-                    {followingsList.length ? (
-                        <List sx={{ width: '100%', maxWidth: 1000, bgcolor: 'background.paper' }}>
-                            {followingsList.map((user, index) => (
-                                <ListItem key={index} alignItems="flex-start">
-                                    <ListItemText
-                                        primary={
-                                            <React.Fragment>
-                                                <Link to={'/' + user.user.username}>
-                                                    <Stack direction="row" spacing={2}>
-                                                        <Avatar alt={user.user.username} src={user.user.icon} />
-                                                        <p>{user.user.username}</p>
-                                                        {props.myProfile?.username ===
-                                                        user.user.username ? null : followingsList.find(
-                                                              (followingUser) =>
-                                                                  followingUser.followedUserId === props.myProfile?.id
-                                                          ) ? (
-                                                            <Button
-                                                                size="small"
-                                                                onClick={(e) => {
-                                                                    updateFollowings(user.user.username, 'unFollow');
-                                                                    e.preventDefault();
-                                                                }}
-                                                            >
-                                                                フォロー解除
-                                                            </Button>
-                                                        ) : (
-                                                            <Button
-                                                                size="small"
-                                                                onClick={(e) => {
-                                                                    updateFollowings(user.user.username, 'follow');
-                                                                    e.preventDefault();
-                                                                }}
-                                                            >
-                                                                フォロー
-                                                            </Button>
-                                                        )}
-                                                    </Stack>
-                                                </Link>
-                                            </React.Fragment>
-                                        }
-                                        secondary={<React.Fragment>{user.user.introduction}</React.Fragment>}
-                                    />
-                                </ListItem>
-                            ))}
-                        </List>
-                    ) : (
-                        <p>やーいぼっち！</p>
-                    )}
-                </TabPanel>
-                <TabPanel value={value} index={1}>
-                    {followersList.length ? (
-                        <List sx={{ width: '100%', maxWidth: 1000, bgcolor: 'background.paper' }}>
-                            {followersList.map((user, index) => (
-                                <ListItem key={index} alignItems="flex-start">
-                                    <ListItemText
-                                        primary={
-                                            <React.Fragment>
-                                                <Link to={'/' + user.user.username}>
-                                                    <Stack direction="row" spacing={2}>
-                                                        <Avatar alt={user.user.username} src={user.user.icon} />
-                                                        <p>{user.user.username}</p>
-                                                        {props.myProfile?.username ===
-                                                        user.user.username ? null : myFollowingsList.find(
-                                                              (myFollowingUser) =>
-                                                                  myFollowingUser.userIdBeingFollowed ===
-                                                                  user.followedUserId
-                                                          ) ? (
-                                                            <Button
-                                                                size="small"
-                                                                onClick={(e) => {
-                                                                    updateFollowings(user.user.username, 'unFollow');
-                                                                    e.preventDefault();
-                                                                }}
-                                                            >
-                                                                フォロー解除
-                                                            </Button>
-                                                        ) : (
-                                                            <Button
-                                                                size="small"
-                                                                onClick={(e) => {
-                                                                    updateFollowings(user.user.username, 'follow');
-                                                                    e.preventDefault();
-                                                                }}
-                                                            >
-                                                                フォロー
-                                                            </Button>
-                                                        )}
-                                                    </Stack>
-                                                </Link>
-                                            </React.Fragment>
-                                        }
-                                        secondary={<React.Fragment>{user.user.introduction}</React.Fragment>}
-                                    />
-                                </ListItem>
-                            ))}
-                        </List>
-                    ) : (
-                        <p>やーいぼっち！</p>
-                    )}
-                </TabPanel>
-            </Box>
-        </>
+      <div
+        role="tabpanel"
+        hidden={currentValue !== tabValue}
+        id={`simple-tabpanel-${tabValue}`}
+        aria-labelledby={`simple-tab-${tabValue}`}
+        {...other}
+      >
+        {currentValue === tabValue && (
+          <Box sx={{ p: 3 }}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
     );
+  }
+
+  return (
+    <>
+      <Box sx={{ width: '100%' }}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs value={props.tabValue} onChange={handleChange} aria-label="basic tabs example">
+            <Tab label="フォロー中" />
+            <Tab label="フォロワー" />
+          </Tabs>
+        </Box>
+        <TabPanel currentValue={props.tabValue} tabValue={'followings'}>
+          {followingsList.length ? (
+            <List sx={{ width: '100%', maxWidth: 1000, bgcolor: 'background.paper' }}>
+              {followingsList.map((user, index) => (
+                <ListItem key={index} alignItems="flex-start">
+                  <ListItemText
+                    primary={
+                      <React.Fragment>
+                        <Link to={'/' + user.user.username}>
+                          <Stack direction="row" spacing={2}>
+                            <Avatar alt={user.user.username} src={user.user.icon} />
+                            <p>{user.user.username}</p>
+                            {props.myProfile?.username === user.user.username ? null : followingsList.find(
+                                (followingUser) => followingUser.followedUserId === props.myProfile?.id
+                              ) ? (
+                              <Button
+                                size="small"
+                                onClick={(e) => {
+                                  updateFollowings(user.user.username, 'unFollow');
+                                  e.preventDefault();
+                                }}
+                              >
+                                フォロー解除
+                              </Button>
+                            ) : (
+                              <Button
+                                size="small"
+                                onClick={(e) => {
+                                  updateFollowings(user.user.username, 'follow');
+                                  e.preventDefault();
+                                }}
+                              >
+                                フォロー
+                              </Button>
+                            )}
+                          </Stack>
+                        </Link>
+                      </React.Fragment>
+                    }
+                    secondary={<React.Fragment>{user.user.introduction}</React.Fragment>}
+                  />
+                </ListItem>
+              ))}
+            </List>
+          ) : (
+            <p>やーいぼっち！</p>
+          )}
+        </TabPanel>
+        <TabPanel currentValue={props.tabValue} tabValue={'followers'}>
+          {followersList.length ? (
+            <List sx={{ width: '100%', maxWidth: 1000, bgcolor: 'background.paper' }}>
+              {followersList.map((user, index) => (
+                <ListItem key={index} alignItems="flex-start">
+                  <ListItemText
+                    primary={
+                      <React.Fragment>
+                        <Link to={'/' + user.user.username}>
+                          <Stack direction="row" spacing={2}>
+                            <Avatar alt={user.user.username} src={user.user.icon} />
+                            <p>{user.user.username}</p>
+                            {props.myProfile?.username === user.user.username ? null : myFollowingsList.find(
+                                (myFollowingUser) => myFollowingUser.userIdBeingFollowed === user.followedUserId
+                              ) ? (
+                              <Button
+                                size="small"
+                                onClick={(e) => {
+                                  updateFollowings(user.user.username, 'unFollow');
+                                  e.preventDefault();
+                                }}
+                              >
+                                フォロー解除
+                              </Button>
+                            ) : (
+                              <Button
+                                size="small"
+                                onClick={(e) => {
+                                  updateFollowings(user.user.username, 'follow');
+                                  e.preventDefault();
+                                }}
+                              >
+                                フォロー
+                              </Button>
+                            )}
+                          </Stack>
+                        </Link>
+                      </React.Fragment>
+                    }
+                    secondary={<React.Fragment>{user.user.introduction}</React.Fragment>}
+                  />
+                </ListItem>
+              ))}
+            </List>
+          ) : (
+            <p>やーいぼっち！</p>
+          )}
+        </TabPanel>
+      </Box>
+    </>
+  );
 };
 
 export default Follow;
--- front/src/Main.tsx
+++ front/src/Main.tsx
@@ -15,40 +15,38 @@ import Follow from './Follow';
 import SettingsProfile from './SettingsProfile';
 
 const Main: React.FC = () => {
-    const callApi = useCallApi();
-    const [myProfile, setMyProfile] = useState<ProfileType>();
+  const callApi = useCallApi();
+  const [myProfile, setMyProfile] = useState<ProfileType>();
 
-    function getAndSetProfile() {
-        getProfile()?.then(setMyProfile);
-    }
+  function getAndSetProfile() {
+    getProfile()?.then(setMyProfile);
+  }
 
-    function getProfile(): Promise<ProfileType> | undefined {
-        return callApi('http://localhost:5000/api/zetter/profile/login');
-    }
+  function getProfile(): Promise<ProfileType> | undefined {
+    return callApi('http://localhost:5000/api/zetter/profile/login');
+  }
 
-    return (
-        <Box sx={{ display: 'flex' }}>
-            <CssBaseline />
-            {myProfile ? <Sidebar myProfile={myProfile} /> : <></>}
-            <Box component="main" sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}>
-                <Routes>
-                    <Route path="login" element={<Login afterLogin={() => getAndSetProfile()} />} />
-                    <Route path="/" element={<Home />} />
-                    <Route path="tweet/:tweetId" element={<TweetDetail />} />
-                    <Route path="notifications" element={<Notifications />} />
-                    <Route path=":username" element={<ProfilePage myProfile={myProfile} />} />
-                    <Route path=":username/followings" element={<Follow myProfile={myProfile} />} />
-                    <Route path=":username/followers" element={<Follow myProfile={myProfile} />} />
-                    <Route
-                        path="settings/:username"
-                        element={
-                            <SettingsProfile myProfile={myProfile} afterUpdateProfile={() => getAndSetProfile()} />
-                        }
-                    />
-                </Routes>
-            </Box>
-        </Box>
-    );
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      {myProfile ? <Sidebar myProfile={myProfile} /> : <></>}
+      <Box component="main" sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}>
+        <Routes>
+          <Route path="login" element={<Login afterLogin={() => getAndSetProfile()} />} />
+          <Route path="/" element={<Home />} />
+          <Route path="tweet/:tweetId" element={<TweetDetail />} />
+          <Route path="notifications" element={<Notifications />} />
+          <Route path=":username" element={<ProfilePage myProfile={myProfile} />} />
+          <Route path=":username/followings" element={<Follow myProfile={myProfile} tabValue="followings" />} />
+          <Route path=":username/followers" element={<Follow myProfile={myProfile} tabValue="followers" />} />
+          <Route
+            path="settings/:username"
+            element={<SettingsProfile myProfile={myProfile} afterUpdateProfile={() => getAndSetProfile()} />}
+          />
+        </Routes>
+      </Box>
+    </Box>
+  );
 };
 
 export default Main;
