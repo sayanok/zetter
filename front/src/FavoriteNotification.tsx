52675e1da8a78e0325b547073d085d106e38f5d9
Sayano <rng.jhn.osca@gmail.com>
2022-11-05 10:50:36 +0900

    modify
    表示内容一部変更

--- front/src/FavoriteNotification.tsx
+++ front/src/FavoriteNotification.tsx
@@ -1,17 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { TweetType, ProfileType, FavoriteType } from './utils/types';
 import { useCallApi } from './utils/api';
-import ReplyButton from './ReplyButton';
-import FavButton from './FavButton';
 
 import Avatar from '@mui/material/Avatar';
-import CompareArrowsIcon from '@mui/icons-material/CompareArrows';
-import IosShareIcon from '@mui/icons-material/IosShare';
 import Divider from '@mui/material/Divider';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemIcon from '@mui/material/ListItemIcon';
-import Button from '@mui/material/Button';
 import StarIcon from '@mui/icons-material/Star';
 
 type FavoriteNotificationProps = {
@@ -22,11 +17,6 @@ type FavoriteNotificationProps = {
 
 const FavoriteNotification: React.FC<FavoriteNotificationProps> = (props) => {
     const callApi = useCallApi();
-    const [myProfile, setMyProfile] = useState<ProfileType>();
-
-    useEffect(() => {
-        getMyProfile()?.then(setMyProfile);
-    }, []);
 
     function getMyProfile(): Promise<ProfileType> | undefined {
         return callApi('http://localhost:5000/api/zetter/profile');
@@ -53,24 +43,6 @@ const FavoriteNotification: React.FC<FavoriteNotificationProps> = (props) => {
                     secondary={
                         <>
                             {props.favorite.tweet.content}
-                            <br />
-                            <ListItemIcon>
-                                <ReplyButton
-                                    tweet={props.favorite.tweet}
-                                    afterPostTweet={() => props.afterPostTweet()}
-                                />
-                                <Button variant="text">
-                                    <CompareArrowsIcon />
-                                </Button>
-                                <FavButton
-                                    numberOfFavorite={props.favorite.tweet.numberOfFavorite}
-                                    isFavorite={props.favorite.userId === myProfile?.id}
-                                    onClick={() => props.updateFavoriteState(props.favorite.tweet)}
-                                />
-                                <Button variant="text">
-                                    <IosShareIcon />
-                                </Button>
-                            </ListItemIcon>
                             <Divider />
                         </>
                     }
